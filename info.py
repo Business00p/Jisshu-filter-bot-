8c7aeb1f364edbdd7960a2f9987fcc98f5d3141a
Tv show <businesskaro0565@gmail.com>
2025-07-03 22:08:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,7 +49,7 @@ DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://tolkkirshencw19938:Vwn
 DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
 
 # Files index database url
-FILES_DATABASE = environ.get("FILES_DATABASE", "")
+FILES_DATABASE = environ.get("FILES_DATABASE", "https://t.me/+LKJfIvNG1QYwYWM1")
 COLLECTION_NAME = environ.get("COLLECTION_NAME", "jisshu")
 
 # Other Channel's
